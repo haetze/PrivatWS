949010bcd90e3f7c081d4567f7c2856ce15a87f2
Richard Stewing <richard.stewing@udo.edu>
2020-08-16 20:57:47 +0200

    SHA-256-Seite

--- src/config.el
+++ src/config.el
@@ -1,8 +1,15 @@
-;;; init.el ---
+;;; config.el ---
 ;;; Makes local packages available
 ;;; src export requires emacs-htmlize (https://melpa.org/#/htmlize)
 (package-initialize)
 
+(setq org-src-lang-modes '(("inline-js" . javascript))) ;; js2 if you're fancy
+(defvar org-babel-default-header-args:inline-js
+  '((:results . "html")
+    (:exports . "results")))
+(defun org-babel-execute:inline-js (body _params)
+  (format "<script type=\"text/javascript\">\n%s\n</script>" body))
 
 
+(setq org-confirm-babel-evaluate (lambda (a b) 'nil))
 ;;; init.el ends here
